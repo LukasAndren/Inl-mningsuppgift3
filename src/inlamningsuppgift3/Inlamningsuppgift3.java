883f9c42669f47d4617fb3e327e1fa6f8fcf0697
Lukas Andrén <jlukasaa@gmail.com>
2018-10-26 15:37:19 +0200

    Commit ärLöst/Blanda
    Commit för metoderna som kollar om pusslet är löst och som blandar knapparna

--- src/inlamningsuppgift3/Inlamningsuppgift3.java
+++ src/inlamningsuppgift3/Inlamningsuppgift3.java
@@ -2,20 +2,24 @@ package inlamningsuppgift3;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
-public class Inlamningsuppgift3 extends JFrame {
+public final class Inlamningsuppgift3 extends JFrame {
 
     JButton nyttSpel = new JButton("Nytt spel");
     JPanel p2 = new JPanel();
     JButton blank = new JButton("");
     JPanel p = new JPanel();
     JButton[] knappar = new JButton[15];
+    int[] tal = new int[]{1, 2, 3, 4, 5, 6, 7, 8,
+        9, 10, 11, 12, 13, 14, 15};
+    Boolean vinst = false;
 
     public Inlamningsuppgift3() {
         p.setSize(450, 450);
         p.setLayout(new GridLayout(4, 4));
         for (int i = 0; i < 15; i++) {
-            knappar[i] = new JButton(i + 1 + "");
+            knappar[i] = new JButton();
             p.add(knappar[i]);
 
         }
@@ -26,7 +30,35 @@ public class Inlamningsuppgift3 extends JFrame {
         setSize(600, 600);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        blanda();
+        if(ärLöst() == true)
+            System.out.println("löst");
 
+    }
+
+    public void blanda() {
+//        int index, temp;
+//        Random random = new Random();
+//        for (int i = tal.length - 1; i > 0; i--) {
+//            index = random.nextInt(i + 1);
+//            temp = tal[index];
+//            tal[index] = tal[i];
+//            tal[i] = temp;
+//        }
+
+        for (int i = 0; i < 15; i++) {
+            knappar[i].setText(tal[i] + "");
+        }
+    }
+
+    public Boolean ärLöst() {
+        for (int i = 0; i < 15 - 1; i++) {
+            if (Integer.parseInt(knappar[i].getText()) > Integer.parseInt(knappar[i + 1].getText())) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {

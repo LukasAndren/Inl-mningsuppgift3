82c775bc4b7941a4bb6eea16ba24850b0cd2da26
Lukas Andrén <jlukasaa@gmail.com>
2018-10-31 09:11:40 +0100

    Merge för ärLöst

--- src/inlamningsuppgift3/Inlamningsuppgift3.java
+++ src/inlamningsuppgift3/Inlamningsuppgift3.java
@@ -20,10 +20,11 @@ public final class Inlamningsuppgift3 extends JFrame implements ActionListener {
         p.setLayout(new GridLayout(4, 4));
         for (int i = 0; i < 16; i++) {
             knappar[i] = new JButton();
+            knappar[i].setText(i+1 + "");
             knappar[i].addActionListener(this);
             p.add(knappar[i]);
-
         }
+        knappar[15].setText("");
         p.add(knappar[15]);
         add(p);
         nyttSpel.addActionListener(this);
@@ -32,7 +33,7 @@ public final class Inlamningsuppgift3 extends JFrame implements ActionListener {
         setSize(600, 600);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        blanda();
+        //blanda();
 
     }
 
@@ -49,199 +50,166 @@ public final class Inlamningsuppgift3 extends JFrame implements ActionListener {
         for (int i = 0; i < 15; i++) {
             knappar[i].setText(tal[i] + "");
         }
-        
+
         knappar[15].setText("");
     }
 
-    public void ärLöst() {
-        for (int i = 0; i < 15 - 1; i++) {
-            if (Integer.parseInt(knappar[i].getText()) < Integer.parseInt(knappar[i + 1].getText())) {
-                System.out.println("du vann");
-            }
+    public boolean ärLöst() {
+        for (int i = 0; i < 16 - 1; i++) {
+                if (!knappar[i].getText().equals(i+1+"") ) {
+                    return false;
+                }
         }
-
-        
+        return true;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == knappar[0]) {
-            if (knappar[1].getText().equals("")){
+            if (knappar[1].getText().equals("")) {
                 bytPlats(1, 0);
-            }
-            else if (knappar[4].getText().equals("")){
+            } else if (knappar[4].getText().equals("")) {
                 bytPlats(4, 0);
             }
 
         } else if (e.getSource() == knappar[1]) {
             if (knappar[0].getText().equals("")) {
                 bytPlats(0, 1);
-            }
-            else if (knappar[2].getText().equals("")) {
+            } else if (knappar[2].getText().equals("")) {
                 bytPlats(2, 1);
-            }
-            else if (knappar[5].getText().equals("")) {
+            } else if (knappar[5].getText().equals("")) {
                 bytPlats(5, 1);
             }
 
         } else if (e.getSource() == knappar[2]) {
             if (knappar[1].getText().equals("")) {
                 bytPlats(1, 2);
-            }
-            else if (knappar[3].getText().equals("")) {
+            } else if (knappar[3].getText().equals("")) {
                 bytPlats(3, 2);
-            }
-            else if (knappar[6].getText().equals("")) {
+            } else if (knappar[6].getText().equals("")) {
                 bytPlats(6, 2);
             }
 
         } else if (e.getSource() == knappar[3]) {
             if (knappar[2].getText().equals("")) {
                 bytPlats(2, 3);
-            }
-            else if (knappar[7].getText().equals("")) {
+            } else if (knappar[7].getText().equals("")) {
                 bytPlats(7, 3);
             }
 
         } else if (e.getSource() == knappar[4]) {
             if (knappar[0].getText().equals("")) {
                 bytPlats(0, 4);
-            }
-            else if (knappar[5].getText().equals("")) {
+            } else if (knappar[5].getText().equals("")) {
                 bytPlats(5, 4);
-            }
-            else if (knappar[8].getText().equals("")) {
+            } else if (knappar[8].getText().equals("")) {
                 bytPlats(8, 4);
             }
 
         } else if (e.getSource() == knappar[5]) {
             if (knappar[1].getText().equals("")) {
                 bytPlats(1, 5);
-            }
-            else if (knappar[4].getText().equals("")) {
+            } else if (knappar[4].getText().equals("")) {
                 bytPlats(4, 5);
-            }
-            else if (knappar[6].getText().equals("")) {
+            } else if (knappar[6].getText().equals("")) {
                 bytPlats(6, 5);
-            }
-            else if (knappar[9].getText().equals("")) {
+            } else if (knappar[9].getText().equals("")) {
                 bytPlats(9, 5);
             }
 
         } else if (e.getSource() == knappar[6]) {
             if (knappar[2].getText().equals("")) {
                 bytPlats(2, 6);
-            }
-            else if (knappar[5].getText().equals("")) {
+            } else if (knappar[5].getText().equals("")) {
                 bytPlats(5, 6);
-            }
-            else if (knappar[7].getText().equals("")) {
+            } else if (knappar[7].getText().equals("")) {
                 bytPlats(7, 6);
-            }
-            else if (knappar[10].getText().equals("")) {
+            } else if (knappar[10].getText().equals("")) {
                 bytPlats(10, 6);
             }
 
         } else if (e.getSource() == knappar[7]) {
             if (knappar[3].getText().equals("")) {
                 bytPlats(3, 7);
-            }
-            else if (knappar[6].getText().equals("")) {
+            } else if (knappar[6].getText().equals("")) {
                 bytPlats(6, 7);
-            }
-            else if (knappar[11].getText().equals("")) {
+            } else if (knappar[11].getText().equals("")) {
                 bytPlats(11, 7);
             }
 
         } else if (e.getSource() == knappar[8]) {
             if (knappar[4].getText().equals("")) {
                 bytPlats(4, 8);
-            }
-            else if (knappar[9].getText().equals("")) {
+            } else if (knappar[9].getText().equals("")) {
                 bytPlats(9, 8);
-            }
-            else if (knappar[12].getText().equals("")) {
+            } else if (knappar[12].getText().equals("")) {
                 bytPlats(12, 8);
             }
 
         } else if (e.getSource() == knappar[9]) {
             if (knappar[5].getText().equals("")) {
                 bytPlats(5, 9);
-            }
-            else if (knappar[8].getText().equals("")) {
+            } else if (knappar[8].getText().equals("")) {
                 bytPlats(8, 9);
-            }
-            else if (knappar[10].getText().equals("")) {
+            } else if (knappar[10].getText().equals("")) {
                 bytPlats(10, 9);
-            }
-            else if (knappar[13].getText().equals("")) {
+            } else if (knappar[13].getText().equals("")) {
                 bytPlats(13, 9);
             }
 
         } else if (e.getSource() == knappar[10]) {
             if (knappar[6].getText().equals("")) {
                 bytPlats(6, 10);
-            }
-            else if (knappar[9].getText().equals("")) {
+            } else if (knappar[9].getText().equals("")) {
                 bytPlats(9, 10);
-            }
-            else if (knappar[11].getText().equals("")) {
+            } else if (knappar[11].getText().equals("")) {
                 bytPlats(11, 10);
-            }
-            else if (knappar[14].getText().equals("")) {
+            } else if (knappar[14].getText().equals("")) {
                 bytPlats(14, 10);
             }
 
         } else if (e.getSource() == knappar[11]) {
             if (knappar[7].getText().equals("")) {
                 bytPlats(7, 11);
-            }
-            else if (knappar[10].getText().equals("")) {
+            } else if (knappar[10].getText().equals("")) {
                 bytPlats(10, 11);
-            }
-            else if (knappar[15].getText().equals("")) {
+            } else if (knappar[15].getText().equals("")) {
                 bytPlats(15, 11);
             }
 
         } else if (e.getSource() == knappar[12]) {
             if (knappar[8].getText().equals("")) {
                 bytPlats(8, 12);
-            }
-            else if (knappar[13].getText().equals("")) {
+            } else if (knappar[13].getText().equals("")) {
                 bytPlats(13, 12);
             }
 
         } else if (e.getSource() == knappar[13]) {
             if (knappar[9].getText().equals("")) {
                 bytPlats(9, 13);
-            }
-            else if (knappar[12].getText().equals("")) {
+            } else if (knappar[12].getText().equals("")) {
                 bytPlats(12, 13);
-            }
-            else if (knappar[14].getText().equals("")) {
+            } else if (knappar[14].getText().equals("")) {
                 bytPlats(14, 13);
             }
 
         } else if (e.getSource() == knappar[14]) {
             if (knappar[10].getText().equals("")) {
                 bytPlats(10, 14);
-            }
-            else if (knappar[13].getText().equals("")) {
+            } else if (knappar[13].getText().equals("")) {
                 bytPlats(13, 14);
-            }
-            else if (knappar[15].getText().equals("")) {
+            } else if (knappar[15].getText().equals("")) {
                 bytPlats(15, 14);
             }
 
         } else if (e.getSource() == knappar[15]) {
             if (knappar[14].getText().equals("")) {
                 bytPlats(14, 15);
-            }
-            else if (knappar[11].getText().equals("")) {
+            } else if (knappar[11].getText().equals("")) {
                 bytPlats(11, 15);
             }
-            
-        } else if (e.getSource() == nyttSpel){
+
+        } else if (e.getSource() == nyttSpel) {
             blanda();
         }
 
@@ -250,6 +218,10 @@ public final class Inlamningsuppgift3 extends JFrame implements ActionListener {
     public void bytPlats(int tomKnapp, int trycktKnapp) {
         knappar[tomKnapp].setText(knappar[trycktKnapp].getText());
         knappar[trycktKnapp].setText("");
+        if (ärLöst() == true){
+            JOptionPane.showMessageDialog(null, "Grattis, du vann!");
+        }
+        
     }
 
     public static void main(String[] args) {
